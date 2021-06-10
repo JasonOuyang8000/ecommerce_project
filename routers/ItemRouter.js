6daacc5827916e4a6f527a1c97a170bbb1c8a606
Jason Ouyang <jasonouyang8000@gmail.com>
2021-06-10 16:44:32 -0700

    randon items route

--- routers/ItemRouter.js
+++ routers/ItemRouter.js
@@ -3,5 +3,6 @@ const itemController = require('../controllers/ItemController');
 const itemRouter = require('express').Router();
 
 itemRouter.get('/', itemController.getAll);
+itemRouter.get('/rand',itemController.getRandItems);
 
 module.exports = itemRouter;
